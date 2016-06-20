05fdb5f63dba7ef3502cfe9ef9c72c91dfcf7711
b4lmung <b4lmung.m@gmail.com>
2016-06-20 15:27:16 +0900

    Create EmoGcon.meta.js

---
+++ EmoGcon.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        EmoGcon
+// @namespace   gconhub.com
+// @include     http://gconhub.com/*
+// @include     http://www.gconhub.com/*
+// @version     1.9
+// @downloadURL https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
+// @updateURL   https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
+// @grant       none
+// ==/UserScript==
