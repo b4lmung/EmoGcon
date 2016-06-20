a514d241a3289f435128bd4cd6823503b4147552
b4lmung <b4lmung.m@gmail.com>
2016-06-20 15:27:53 +0900

    Update EmoGcon.user.js

--- EmoGcon.user.js
+++ EmoGcon.user.js
@@ -5,9 +5,10 @@
 // @include     http://www.gconhub.com/*
 // @version     1.9
 // @downloadURL https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
-// @updateURL   https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
+// @updateURL   https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.meta.js
 // @grant       none
 // ==/UserScript==
+
 
 
 function clickKanade(zEvent) {
