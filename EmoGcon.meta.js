9ad3166359c63dc6607d790bb2c0d6e4ae1db313
b4lmung <b4lmung.m@gmail.com>
2016-06-23 01:17:31 +0900

    Update EmoGcon.meta.js

--- EmoGcon.meta.js
+++ EmoGcon.meta.js
@@ -3,8 +3,8 @@
 // @namespace   gconhub.com
 // @include     http://gconhub.com/*
 // @include     http://www.gconhub.com/*
-// @version     2.0.1
+// @version     2.0.2
 // @downloadURL https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
-// @updateURL   https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
+// @updateURL   https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.meta.js
 // @grant       none
 // ==/UserScript==
